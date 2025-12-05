96091fe339cc32bbfb8a65592b65079ed6856f3a
GangadharPrathap <23A91A0416@aec.edu.in>
2025-12-06 00:04:15 +0530

    completed the problem2.py

--- python practice/problem2.py
+++ python practice/problem2.py
@@ -1,4 +1,11 @@
 print('Max and Min in the array')
 arr=list(map(int,input('enter the array elements: ').split()))
 mx=arr[0]
-for i in range (arr[1]:):
+mn=arr[0]
+for i in range (1,len(arr)):
+    if arr[i]>mx:
+        mx=arr[i]
+    if arr[i]<mn:
+        mn=arr[i]
+print('Maximum element is:',mx)
+print('Minimum element is:',mn)
